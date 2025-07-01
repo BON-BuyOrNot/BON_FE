789171b25e9e8ad3dc3fe3c8010740706650fb4b
HYUNA <phyunaa0525@gmail.com>
2025-07-01 10:50:26 +0900

    feat :: svg

---
+++ src/assets/feed.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Feed = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect x="4" y="5" width="16" height="2.5" rx="1.25" fill="black" />
+    <rect x="4" y="10.75" width="16" height="2.5" rx="1.25" fill="black" />
+    <rect x="4" y="16.5" width="16" height="2.5" rx="1.25" fill="black" />
+  </svg>
+);
+
+export default Feed;
---
+++ src/assets/following.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Following = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="8" cy="10" r="3" fill="black" />
+    <circle cx="16" cy="10" r="3" fill="black" fillOpacity="0.7" />
+    <ellipse cx="8" cy="17" rx="5" ry="2.5" fill="black" fillOpacity="0.7" />
+    <ellipse cx="16" cy="17" rx="5" ry="2.5" fill="black" fillOpacity="0.4" />
+  </svg>
+);
+
+export default Following;
